2e5b5b5c91d3ef1742004e3c5936ded0008b0fd1
Stanley Darmawan <stanley@interact.technology>
2017-09-27 16:50:33 +1000

    added shared()

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -21,7 +21,7 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        XCUIDevice.shared.orientation = .portrait
+        XCUIDevice.shared().orientation = .portrait
     }
     
     override func tearDown() {
